8b67b264eb8af5cf9d44e60ccc746f2bbe10d472
GrRooman <zubromana744@gmail.com>
2021-08-24 22:01:29 +0300

    Added class for saving data

---
+++ src/WriteToFile.java
@@ -0,0 +1,25 @@
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+
+
+class WriteToFile {
+    FileOutputStream fos;
+    WriteToFile(){
+        
+    }
+    void saveDataInFile(String fname,String strdata){
+        try{
+            fos = new FileOutputStream(fname);
+            byte[] b = strdata.getBytes();
+            fos.write(b); 
+            
+        } catch(IOException e){
+            System.out.println(e);
+        }
+        //finally
+    }
+    
+}
